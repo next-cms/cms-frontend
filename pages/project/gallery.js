d332fcec6ea63e6445809562588a63bc4ec5f289
sazid <sazidmailbox@gmail.com>
2019-10-18 15:12:26 +0600

    gallery fix

--- pages/project/gallery.js
+++ pages/project/gallery.js
@@ -5,14 +5,18 @@ import * as PropTypes from "prop-types";
 import {MenuContext} from "../../contexts/MenuContextProvider";
 import MediaGallery from "../../components/common/media_gallery/MediaGallery";
 import RoutesInfo from "../../constants/RoutesInfo";
+import {useRouter} from "next/router";
 
 const GalleryPage = (props) => {
     const menuContext = React.useContext(MenuContext);
+    const router = useRouter();
 
     React.useEffect(() => {
         menuContext.setSelectedKeys([RoutesInfo.GalleryPage.slug]);
         menuContext.setOpenedKeys([]);
     }, []);
+
+    if (!router.query.projectId) return null;
 
     return (
         <PageWrapper style={{
@@ -21,7 +25,7 @@ const GalleryPage = (props) => {
             minHeight: "calc(100vh - 80px)",
             padding: 0
         }}>
-            <MediaGallery project={props.project}/>
+            <MediaGallery projectId={router.query.projectId}/>
         </PageWrapper>
     );
 };
