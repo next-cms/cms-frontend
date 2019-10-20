2286e54ef2c1cd63b2ce8b126238017161334224
sazid <sazidmailbox@gmail.com>
2019-10-21 00:29:00 +0600

    project pages different process

--- components/editor_components/ProjectPages.js
+++ components/editor_components/ProjectPages.js
@@ -1,22 +1,61 @@
 import React, {Fragment, useContext, useEffect, useState} from "react";
-import {Button, Icon, message, Table} from "antd";
-import {ClientContext, useMutation, useQuery} from "graphql-hooks";
-import {ADD_PAGE, PROJECT_PAGES} from "../../utils/GraphQLConstants";
+import {Button, Divider, Icon, message, Table} from "antd";
+import {useMutation, useQuery} from "graphql-hooks";
+import {ADD_PAGE, DELETE_PAGE, PROJECT_PAGES} from "../../utils/GraphQLConstants";
 import {handleGraphQLAPIErrors} from "../../utils/helpers";
 import Link from "next/link";
 import RoutesInfo from "../../constants/RoutesInfo";
 import Router from "next/router";
+import {DataStoreContext} from "../../contexts/DataStoreContextProvider";
 
-const ProjectDataStore = ({projectId}) => {
+const ProjectPages = ({projectId}) => {
     const [skip, setSkip] = useState(0);
     const [pageSize, setPageSize] = useState(100);
+    const dataStoreContext = useContext(DataStoreContext);
 
     const {loading, error, data, refetch} = useQuery(PROJECT_PAGES, {
         variables: {projectId, skip, limit: pageSize},
         skipCache: true
     });
-    const graphQLClient = useContext(ClientContext);
     const [addPage] = useMutation(ADD_PAGE);
+    const [deletePage] = useMutation(DELETE_PAGE);
+
+    const onDeletePage = async (page) => {
+        const result = await deletePage({
+            variables: {
+                projectId: projectId,
+                page: page.slug
+            }
+        });
+        if (!result.error) {
+            refetch({variables: {projectId: projectId}});
+            message.success(`Deleted page '${page.Title}' successfully!`);
+        } else {
+            handleGraphQLAPIErrors(result.error);
+        }
+    };
+
+    const onAddPageClick = async (e) => {
+        const result = await addPage({
+            variables: {
+                projectId: projectId
+            }
+        });
+        if (!result.error) {
+            const newPage = result.data.addPage;
+            Router.push(`${RoutesInfo.ProjectPages.path}?projectId=${projectId}&pageName=${newPage.slug}`,
+                `${RoutesInfo.ProjectPages.path}/?projectId=${projectId}&pageName=${newPage.slug}`);
+        } else {
+            handleGraphQLAPIErrors(result.error);
+        }
+    };
+
+    useEffect(() => {
+        if (dataStoreContext.projectPagesListUpdated) {
+            refetch({variables: {projectId: projectId}});
+            dataStoreContext.setProjectPagesListUpdated(false);
+        }
+    }, [dataStoreContext.projectPagesListUpdated]);
 
     useEffect(() => {
         if (error) {
@@ -58,11 +97,11 @@ const ProjectDataStore = ({projectId}) => {
                             <Icon style={{color: "blue"}} type="edit"/>
                         </a>
                     </Link>
-                    {/*<Divider type="vertical"/>*/}
+                    <Divider type="vertical"/>
                     {/*<Fragment>*/}
-                    {/*<a onClick={() => handleClick(record)}>*/}
-                    {/*<Icon style={{color: "red"}} type="delete"/>*/}
-                    {/*</a>*/}
+                    <a onClick={() => onDeletePage(record)}>
+                        <Icon style={{color: "red"}} type="delete"/>
+                    </a>
 
                     {/*</Fragment>*/}
                 </span>
@@ -70,27 +109,14 @@ const ProjectDataStore = ({projectId}) => {
         }
     ];
 
-    const onAddPageClick = async (e) => {
-        const result = await addPage({
-            variables: {
-                projectId: projectId
-            }
-        });
-        if (!result.error) {
-            const newPage = data.addPage;
-            Router.push(`${RoutesInfo.ProjectPages.slug}?projectId=${projectId}&pageName=${newPage.slug}`,
-                `${RoutesInfo.ProjectPages.slug}/?projectId=${projectId}&pageName=${newPage.slug}`);
-        } else {
-            handleGraphQLAPIErrors(result.error);
-        }
-    };
-
     return (
         <Fragment>
-            <Button type="primary" onClick={onAddPageClick}>Add Page</Button>
+            <div>
+                <Button type="primary" onClick={onAddPageClick}>Add Page</Button>
+            </div>
             <Table dataSource={allPages} columns={columns} rowKey={"slug"}/>
         </Fragment>
     );
 };
 
-export default ProjectDataStore;
+export default ProjectPages;
